4bff353660e513554d7dd6b27046a968bb0fcd96
kavindya <gayathrikavindhya@gmail.com>
2021-01-15 22:09:51 +0530

    auth pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React,{Fragment} from 'react'
 import './App.css';
 import  {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
-
 import NavbarPage from './components/layouts/Navbar';
-import SignInSide from './components/auth/login'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
+import ForgotPassword from './components/auth/ForgotPassword';
 
 
 const App=()=> {
   return (
 
-
 <Router>
 <NavbarPage/>
-<SignInSide/>
+<Switch>
+<Route path='/login' exact render={props => <SignIn {...props} />} />
+<Route path='/signup' exact render={props => <SignUp {...props} />} />
+<Route path='/forgotpassword' exact render={props => <ForgotPassword {...props} />} />
+</Switch>
 </Router>
-
-
-
 
   );
 }
